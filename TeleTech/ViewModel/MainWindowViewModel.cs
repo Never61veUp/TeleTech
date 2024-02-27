8b4630b638a97af3170ed661f44f06b24c7b91a3
Never61veUp <savageseed@yandex.ru>
2024-02-27 23:01:24 +0500

    Улучшил навигацию + анимации

--- TeleTech/ViewModel/MainWindowViewModel.cs
+++ TeleTech/ViewModel/MainWindowViewModel.cs
@@ -10,8 +10,23 @@ namespace TeleTech.ViewModel
 {
     internal class MainWindowViewModel : ViewModelBase
     {
-        public ICommand OpenUsersPage {  get; set; }
+        #region ICommand
+        public ICommand OpenHomePageCommand { get; set; }
+        public ICommand OpenUsersPageCommand {  get; set; }
+        public ICommand OpenTariffPageCommand { get; set; }
+        public ICommand OpenSettingsPageCommand { get; set; }
+        #endregion
         private object _page;
+        public object Page { 
+            get 
+            { 
+                return _page; 
+            } 
+            set 
+            { 
+                _page = value; OnPropertyChanged("Page"); 
+            } 
+        }
         private string _userAvatar;
         public string UserAvatar { 
             get
@@ -26,13 +41,20 @@ namespace TeleTech.ViewModel
         }
         private string _userName;
         public string UserName { get => _userName; set => _userName = value; }
+
         private string _userGeoStatus;
         public string UserGeoStatus { get => _userGeoStatus; set => _userGeoStatus = value; }
-        public object Page { get { return _page; } set { _page = value;OnPropertyChanged(); } }
-        private void Home(object obj) => Page = new UsersViewModel();
+        
+        private void OpenHomePageMethod(object obj) => Page = new HomeViewModel();
+        private void OpenUsersPageMethod(object obj) => Page = new UsersViewModel();
+        private void OpenTariffPageMethod(object obj) => Page = new TariffViewModel();
+        private void OpenSettingsPageMethod(object obj) => Page = new SettingsViewModel();
         public MainWindowViewModel()
         {
-            OpenUsersPage = new RelayCommand(Home);
+            OpenHomePageCommand = new RelayCommand(OpenHomePageMethod);
+            OpenUsersPageCommand = new RelayCommand(OpenUsersPageMethod);
+            OpenTariffPageCommand = new RelayCommand(OpenTariffPageMethod);
+            OpenSettingsPageCommand = new RelayCommand(OpenSettingsPageMethod);
             UserAvatar = "/Assets/Icons/ava1.png";
             UserName = "Евгений Картов";
             UserGeoStatus = "Москва, Россия";
