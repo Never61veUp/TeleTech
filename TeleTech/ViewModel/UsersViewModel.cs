77b49adb1d58197d450908e5bbcd19cab2001e9a
Never61veUp <savageseed@yandex.ru>
2024-03-05 19:48:54 +0500

    поиск и фильтр

--- TeleTech/ViewModel/UsersViewModel.cs
+++ TeleTech/ViewModel/UsersViewModel.cs
@@ -1,12 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Intrinsics.Arm;
 using System.Text;
 using System.Threading.Tasks;
+using TeleTech.Model;
 
 namespace TeleTech.ViewModel
 {
     internal class UsersViewModel : ViewModelBase
     {
+        ArmContext armContext = new ArmContext();
+        public string CountUsers { get; set; }
+        public UsersViewModel()
+        {
+            CountUsers = $"{armContext.Users.Count()} пользователей"; 
+        }
     }
 }
