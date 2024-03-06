547877726076ff99babe32800a8b90ca06f9cecc
Never61veUp <savageseed@yandex.ru>
2024-03-06 16:54:20 +0500

    данные в датагрид

--- TeleTech/ViewModel/UsersViewModel.cs
+++ TeleTech/ViewModel/UsersViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Runtime.Intrinsics.Arm;
 using System.Text;
@@ -11,10 +12,31 @@ namespace TeleTech.ViewModel
     internal class UsersViewModel : ViewModelBase
     {
         ArmContext armContext = new ArmContext();
+
+
+        List<Simissuance> simissuances;
+        List<User> users;
+
         public string CountUsers { get; set; }
+        public List<UserExtended> UsersWithSIMs {  get; set; }
+
+
         public UsersViewModel()
         {
-            CountUsers = $"{armContext.Users.Count()} пользователей"; 
+            users = armContext.Users.ToList();
+            simissuances = armContext.Simissuances.ToList();
+
+            var userSims = from user in users
+                           join sim in simissuances on user.PassportId equals sim.PassportNumber
+                           select new UserExtended { Name = user.Name, SimCardNumber = sim.SimcardNumber, PassportId = user.PassportId, Id = user. };
+
+            UsersWithSIMs = userSims.ToList();
+
+
+
+
+
+            CountUsers = $"{armContext.Users.Count()} пользователей";
         }
     }
 }
