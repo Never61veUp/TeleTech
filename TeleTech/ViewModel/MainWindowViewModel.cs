85018630e38cf0f5c789e1e11ec9112da831bd07
Алексей <savageseed@yandex.ru>
2024-04-15 23:54:23 +0500

    Получение данных сотрудника с класса, вместо отдельных свойств

--- TeleTech/ViewModel/MainWindowViewModel.cs
+++ TeleTech/ViewModel/MainWindowViewModel.cs
@@ -2,69 +2,25 @@
 using System.Windows.Input;
 using System.Windows.Media;
 using TeleTech.Commands;
+using TeleTech.Model;
 using TeleTech.Stores;
 
 namespace TeleTech.ViewModel
 {
     internal class MainWindowViewModel : ViewModelBase
     {
-        #region ICommand
-        public ICommand HomeCommand { get; }
-        public ICommand UsersCommand { get; }
-        public ICommand TariffCommand { get; }
-        public ICommand SettingsCommand { get; }
-        public ICommand SingOutCommand { get; }
-        public ICommand CloseDialogWindowCommand { get; }
-        #endregion
-
-        public string? UserName => _accountStore.CurrentAccount?.FirstName;
-        public string? UserGeoStatus => _accountStore.CurrentAccount?.Location;
-        public char? UserCharacter => _accountStore.CurrentAccount?.Character;
-        public Brush? UserAvatarColor => _accountStore.CurrentAccount?.BgColor;
-
-        public ViewModelBase? CurrentView => _navigationStore.CurrentView;
-
-        public ViewModelBase? CurrentDialog => _navigationStore.CurrentDialog;
-
+        
         private bool _isAppActive = true;
-        public bool IsAppActive
-        {
-            get { return _isAppActive; }
-            set
-            {
-                _isAppActive = value;
-                OnPropertyChanged(nameof(IsAppActive));
-            }
-        }
         private bool _isLeftBarVisible;
-        public bool IsLeftBarVisible
-        {
-            get { return _isLeftBarVisible; }
-            set
-            {
-                _isLeftBarVisible = value;
-                OnPropertyChanged(nameof(IsLeftBarVisible));
-            }
-        }
         private Visibility _isDialogOpen = Visibility.Collapsed;
-        public Visibility IsDialogOpen
-        {
-            get { return _isDialogOpen; }
-            set
-            {
-                _isDialogOpen = value;
-                OnPropertyChanged(nameof(IsDialogOpen));
-            }
-        }
-
-
 
         private readonly NavigationStore _navigationStore;
         private readonly AccountStore _accountStore;
+        
 
         public MainWindowViewModel(NavigationStore navigationStore, AccountStore accountStore)
         {
-
+            
 
             _navigationStore = navigationStore;
             _navigationStore.CurrentViewChanged += OnCurrentViewChanged;
@@ -85,9 +41,51 @@ namespace TeleTech.ViewModel
             UsersViewModel.AddNewClientCommand = new ShowDialogCommand<AddNewClientViewModel>(_navigationStore,
                 () => new AddNewClientViewModel(_accountStore), this);
             CloseDialogWindowCommand = new ShowDialogCommand<AddNewClientViewModel>(_navigationStore, () => null, this);
+            
+
+
         }
+        #region ICommand
+        public ICommand HomeCommand { get; }
+        public ICommand UsersCommand { get; }
+        public ICommand TariffCommand { get; }
+        public ICommand SettingsCommand { get; }
+        public ICommand SingOutCommand { get; }
+        public ICommand CloseDialogWindowCommand { get; }
+        #endregion
 
+        public EmployeeExtended CurrentEmployee => _accountStore.CurrentAccount;
 
+        public ViewModelBase? CurrentView => _navigationStore.CurrentView;
+        public ViewModelBase? CurrentDialog => _navigationStore.CurrentDialog;
+
+        public bool IsAppActive
+        {
+            get { return _isAppActive; }
+            set
+            {
+                _isAppActive = value;
+                OnPropertyChanged(nameof(IsAppActive));
+            }
+        }
+        public bool IsLeftBarVisible
+        {
+            get { return _isLeftBarVisible; }
+            set
+            {
+                _isLeftBarVisible = value;
+                OnPropertyChanged(nameof(IsLeftBarVisible));
+            }
+        }
+        public Visibility IsDialogOpen
+        {
+            get { return _isDialogOpen; }
+            set
+            {
+                _isDialogOpen = value;
+                OnPropertyChanged(nameof(IsDialogOpen));
+            }
+        }
 
         private void OnCurrentDialogChanged()
         {
@@ -110,10 +108,7 @@ namespace TeleTech.ViewModel
 
         private void AccountStore_CurrentAccountChanged()
         {
-            OnPropertyChanged(nameof(UserName));
-            OnPropertyChanged(nameof(UserGeoStatus));
-            OnPropertyChanged(nameof(UserCharacter));
-            OnPropertyChanged(nameof(UserAvatarColor));
+            OnPropertyChanged(nameof(CurrentEmployee));
             if (_accountStore.CurrentAccount != null)
                 IsLeftBarVisible = true;
 
